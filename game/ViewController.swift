759bd093e48e55ebc86e1effcf2c36818060fd92
Mustafa Ali KILCI <mustafa_klc997@hotmail.com>
2022-11-20 23:01:03 +0300

    Initial Commit

---
+++ game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  game
+//
+//  Created by Mustafa Ali KILCI on 20.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
